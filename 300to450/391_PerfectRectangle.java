7303da10ad1add7aefeccfec12a05a6349ac839e
mylife126 <shenxin6@hotmail.com>
2020-01-22 21:58:40 -0500

    added new solutions

---
+++ 300to450/391_PerfectRectangle.java
@@ -0,0 +1,65 @@
+/*
+思路是，判断是否是一个完整的大长方形：
+1. 所有小长方形的area Sum == 大长方形的面积 （large x - smallx） * (largey - smally)
+2. 最后只能存在四个最大的pivot顶点，
+3. 内部的顶点出现的次数一定是偶数次，这样他们在hashTable里出现的次数是0次。 出现 《-》 删除 是一次偶数pair
+4. 最后set里出现的只能是四个最大的顶点 不然就是出现了 overlap的情况， 使得内部的顶点成了奇数次
+*/
+
+class Solution {
+    public boolean isRectangleCover(int[][] rectangles) {
+        if (rectangles.length == 0 || rectangles[0].length == 0) return false;
+        /*
+        建立running Maxx maxy minx miny
+        */
+        int x1 = Integer.MAX_VALUE;
+        int x2 = Integer.MIN_VALUE;
+        int y1 = Integer.MAX_VALUE;
+        int y2 = Integer.MIN_VALUE;
+        /*
+        记录顶点的情况
+        */
+        HashSet<String> set = new HashSet<String>();
+        //记录 runningArea of all rectangles
+        int area = 0;
+
+        for (int[] rect : rectangles) {
+            x1 = Math.min(rect[0], x1);
+            y1 = Math.min(rect[1], y1);
+            x2 = Math.max(rect[2], x2);
+            y2 = Math.max(rect[3], y2);
+
+            area += (rect[2] - rect[0]) * (rect[3] - rect[1]);
+            /*
+            找到四个顶点
+            */
+            String s1 = rect[0] + " " + rect[1];
+            String s2 = rect[0] + " " + rect[3];
+            String s3 = rect[2] + " " + rect[3];
+            String s4 = rect[2] + " " + rect[1];
+            
+            // if (!set.add(s1)) set.remove(s1);
+            // if (!set.add(s2)) set.remove(s2);
+            // if (!set.add(s3)) set.remove(s3);
+            // if (!set.add(s4)) set.remove(s4);
+            addOrDelete(set, s1);
+            addOrDelete(set, s2);
+            addOrDelete(set, s3);
+            addOrDelete(set, s4);
+            
+        }
+
+        if (!set.contains(x1 + " " + y1) || !set.contains(x1 + " " + y2) || 
+            !set.contains(x2 + " " + y1) || !set.contains(x2 + " " + y2) || 
+            set.size() != 4) return false;
+
+        return area == (x2-x1) * (y2-y1);
+    }
+    private void addOrDelete(Set<String> set, String a){
+        if (set.contains(a)){
+            set.remove(a);
+        } else {
+            set.add(a);
+        }
+    }
+}
